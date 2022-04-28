6b31ad8da3e6bbf0c68f3fd95d1c2d40ba65a9a5
René Monroy <rene.monroy@gmail.com>
2022-04-27 23:34:40 -0500

    Use React 17

--- src/index.js
+++ src/index.js
@@ -1,59 +1,23 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import { BrowserRouter } from "react-router-dom";
-// import { Provider as ReduxProvider } from "react-redux";
-// import { CssBaseline } from "@mui/material";
-// import { ThemeProvider as MUIThemeProvider } from "@mui/material/styles";
-// import AdapterDateFns from "@mui/lab/AdapterDateFns";
-// import LocalizationProvider from "@mui/lab/LocalizationProvider";
-// import reportWebVitals from "./reportWebVitals";
-// import App from "./App";
-// import { MUITheme } from "./theme";
-// import store from "./store/store";
-// import "./index.css";
-
-// console.log(
-//   "process.env.REACT_APP_ENV_API_KEY:",
-//   process.env.REACT_APP_ENV_API_KEY
-// );
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <ReduxProvider store={store}>
-//       <CssBaseline />
-//       <MUIThemeProvider theme={MUITheme}>
-//         <LocalizationProvider dateAdapter={AdapterDateFns}>
-//           <BrowserRouter>
-//             <App />
-//           </BrowserRouter>
-//         </LocalizationProvider>
-//       </MUIThemeProvider>
-//     </ReduxProvider>
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
-
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider as ReduxProvider } from "react-redux";
 import { CssBaseline } from "@mui/material";
 import { ThemeProvider as MUIThemeProvider } from "@mui/material/styles";
 import AdapterDateFns from "@mui/lab/AdapterDateFns";
 import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import reportWebVitals from "./reportWebVitals";
 import App from "./App";
 import { MUITheme } from "./theme";
 import store from "./store/store";
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+console.log(
+  "process.env.REACT_APP_ENV_API_KEY:",
+  process.env.REACT_APP_ENV_API_KEY
+);
+
+ReactDOM.render(
   <React.StrictMode>
     <ReduxProvider store={store}>
       <CssBaseline />
@@ -65,7 +29,8 @@ root.render(
         </LocalizationProvider>
       </MUIThemeProvider>
     </ReduxProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function

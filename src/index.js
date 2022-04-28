77662e88ed6b4a34d43478525ea5d5d2006fd381
René Monroy <rene.monroy@gmail.com>
2022-04-27 19:15:07 -0500

    Add store, theme and router providers

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider as ReduxProvider } from "react-redux";
+import { CssBaseline } from "@mui/material";
+import { ThemeProvider as MUIThemeProvider } from "@mui/material/styles";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import reportWebVitals from "./reportWebVitals";
+import App from "./App";
+import { MUITheme } from "./theme";
+import store from "./store/store";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ReduxProvider store={store}>
+      <CssBaseline />
+      <MUIThemeProvider theme={MUITheme}>
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </LocalizationProvider>
+      </MUIThemeProvider>
+    </ReduxProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function

69b91f3f5623f1d59078eef8ead65afef35a7fdc
René Monroy <rene.monroy@gmail.com>
2022-04-27 19:14:25 -0500

    Render App content

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Amplify } from "aws-amplify";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import awsExports from "./aws-exports";
+import UILayout from "./components/UILayout/UILayout";
+import ProductsView from "./views/Products/Products";
+import ProductView from "./views/Product/Product";
+import ProductSelection from "./components/ProductSelection/ProductSelection";
+import AuthHeader from "./components/AuthHeader/AuthHeader";
+import AuthFooter from "./components/AuthFooter/AuthFooter";
+import SignInHeader from "./components/SignInHeader/SignInHeader";
+import SignInFooter from "./components/SignInFooter/SignInFooter";
+
+Amplify.configure(awsExports);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<UILayout />}>
+        <Route path="products" element={<ProductsView />}>
+          <Route index element={<ProductSelection />} />
+          <Route path=":id" element={<ProductView />} />
+        </Route>
+        <Route index element={<Navigate replace to="products" />} />
+      </Route>
+    </Routes>
   );
 }
 
-export default App;
+export default withAuthenticator(App, {
+  components: {
+    Header: AuthHeader,
+    Footer: AuthFooter,
+    SignIn: {
+      Header: SignInHeader,
+      Footer: SignInFooter,
+    },
+  },
+});

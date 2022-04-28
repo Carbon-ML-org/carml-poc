59a92203e00e9dc51d3e37f98526ea547d442bec
René Monroy <rene.monroy@gmail.com>
2022-04-27 23:33:47 -0500

    Use defaultCenter instead center at UIMap component

--- src/components/UIMap/UIMap.jsx
+++ src/components/UIMap/UIMap.jsx
@@ -1,5 +1,10 @@
 import GoogleMapReact from "google-map-react";
 
+console.log(
+  "REACT_APP_GOOGLE_MAPS_KEY:",
+  process.env.REACT_APP_GOOGLE_MAPS_KEY
+);
+
 /**
  * UIMap
  * -----------------------------------------------------------
@@ -9,7 +14,7 @@ export default function UIMap({ zoom = 8, lat, lng }) {
     <div style={{ height: "130px", width: "100%" }}>
       <GoogleMapReact
         bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_KEY }}
-        center={{ lat, lng }}
+        defaultCenter={{ lat, lng }}
         defaultZoom={zoom}
       />
     </div>

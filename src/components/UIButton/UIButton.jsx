5f9aabac6392e835d5f1b6b57e0eff647f4e8182
René Monroy <rene.monroy@gmail.com>
2022-04-27 19:10:20 -0500

    Create UIButton component

---
+++ src/components/UIButton/UIButton.jsx
@@ -0,0 +1,9 @@
+import { Button } from "@mui/material";
+
+/**
+ * UIButton
+ * -----------------------------------------------------------
+ */
+export default function UIButton(props) {
+  return <Button disableElevation variant="contained" {...props} />;
+}

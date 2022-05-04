7baa5795a8b1b86035452ffd02ddabce6d8dec24
René Monroy <rene.monroy@gmail.com>
2022-05-04 18:17:31 -0500

    Create AppMain component

---
+++ src/components/AppMain/AppMain.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { SAppMain } from "./AppMain.styles";
+
+function AppMain({ children }) {
+  return <SAppMain>{children}</SAppMain>;
+}
+
+export default AppMain;
---
+++ src/components/AppMain/AppMain.styles.js
@@ -0,0 +1,27 @@
+import styled from "@emotion/styled";
+
+export const SAppMain = styled.main`
+  align-items: stretch;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 2rem;
+  padding-top: 4rem;
+  position: relative;
+
+  & > * {
+    padding-top: 2rem;
+  }
+
+  & > :first-child {
+    flex-grow: 1;
+    padding-left: 2rem;
+  }
+
+  & > :last-child {
+    flex-basis: 0;
+    flex-grow: 999;
+    min-inline-size: 50%;
+    padding-right: 2rem;
+  }
+`;

e9667ef6d9367a6cabce2187f13c15dea2da0861
René Monroy <rene.monroy@gmail.com>
2022-05-06 14:53:04 -0500

    Update AppSidebar to include main elements

--- src/components/AppSidebar/AppSidebar.jsx
+++ src/components/AppSidebar/AppSidebar.jsx
@@ -1,12 +1,32 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { Button, List, ListItem } from "@mui/material";
+import AppUsername from "../AppUsername/AppUsername";
 import { SAppSidebar, SAppSidebarHeader } from "./AppSidebar.styles";
 
-function AppSidebar({ open = true }) {
+function AppSidebar() {
+  const { sidebarIsOpen, sidebarWidth, headerHeight } = useSelector(
+    (state) => state.ui
+  );
+  console.log("sidebarIsOpen:", sidebarIsOpen);
+
   return (
-    <SAppSidebar anchor="left" open={open} variant="persistent">
-      <SAppSidebarHeader>
-        <p>User here.</p>
+    <SAppSidebar
+      anchor="left"
+      open={sidebarIsOpen}
+      variant="persistent"
+      width={sidebarWidth}
+    >
+      <SAppSidebarHeader headerHeight={headerHeight}>
+        <AppUsername />
       </SAppSidebarHeader>
+      <List>
+        <ListItem>
+          <Button disableElevation fullWidth variant="contained">
+            New Message Type
+          </Button>
+        </ListItem>
+      </List>
     </SAppSidebar>
   );
 }
--- src/components/AppSidebar/AppSidebar.styles.js
+++ src/components/AppSidebar/AppSidebar.styles.js
@@ -1,17 +1,37 @@
 import styled from "@emotion/styled";
 import { Drawer } from "@mui/material";
+import { darkestBgColor, darkBgColor } from "../../theme";
 
-export const SAppSidebar = styled(Drawer)`
-  width: 15rem;
+export const SAppSidebar = styled(Drawer, {
+  shouldForwardProp: (prop) => prop !== "width",
+})`
+  --app-sidebar-width: ${({ width }) => width}rem;
+
   flex-shrink: 0;
+  width: var(--app-sidebar-width);
 
   & .MuiDrawer-paper {
-    width: 15rem;
+    background-color: ${darkestBgColor};
     box-sizing: border-box;
+    width: var(--app-sidebar-width);
   }
 `;
 
 export const SAppSidebarHeader = styled.div`
+  align-items: center;
+  background-color: ${darkBgColor};
   display: flex;
-  alignitems: center;
+  justify-content: justify-content;
+  height: ${({ headerHeight }) => headerHeight}rem;
+
+  & .MuiCardHeader-content {
+    align-items: flex-start;
+    color: #ffffff;
+    display: flex;
+    flex-direction: column;
+  }
+
+  & .MuiCardHeader-action {
+    align-self: auto;
+  }
 `;

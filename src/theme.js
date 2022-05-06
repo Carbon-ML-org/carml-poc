8d3b842385d4f0d2f34da6dab08033fad3eaff77
René Monroy <rene.monroy@gmail.com>
2022-05-06 14:45:11 -0500

    Update colors for theme

--- src/theme.js
+++ src/theme.js
@@ -1,8 +1,11 @@
 import { createTheme as createMUITheme } from "@mui/material/styles";
 
 const themeName = "light";
-const primaryColor = "#655bef";
-const secondaryColor = "#49d8cd";
+export const primaryColor = "#556ff6";
+export const secondaryColor = "#49d8cd";
+export const darkestBgColor = "#141639";
+export const darkBgColor = "#2f315b";
+export const darkColor = "#a2b1ff";
 
 /**
  * MUI Theme

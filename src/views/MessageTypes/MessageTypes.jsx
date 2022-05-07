4ac605dd63bffc613111091fae8e79e970a8858e
René Monroy <rene.monroy@gmail.com>
2022-05-07 01:26:26 -0500

    Render Message Types in a table list

--- src/views/MessageTypes/MessageTypes.jsx
+++ src/views/MessageTypes/MessageTypes.jsx
@@ -1,42 +1,13 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Box, CircularProgress } from "@mui/material";
-import { SMessageTypes } from "./MessageTypes.styles";
-import { getMessageTypes } from "../../store/messageTypes/messageTypes.actions";
+import { Container } from "@mui/material";
+import React from "react";
+import MessageTypesTable from "./MessageTypesTable";
 
 function MessageTypes() {
-  const dispatch = useDispatch();
-  const { messageTypes, status } = useSelector((state) => state.messageTypes);
-
-  console.log("messageTypes:", messageTypes);
-  console.log("status:", status);
-
-  useEffect(() => {
-    dispatch(getMessageTypes());
-  }, []); // eslint-disable-line
-
-  if (status === "success") {
-    return (
-      <SMessageTypes>
-        <p>Create Message Type here...</p>
-      </SMessageTypes>
-    );
-  } else if (status === "failed") {
-    return <p>Not found!</p>;
-  } else {
-    return (
-      <Box
-        sx={{
-          alignItems: "center",
-          display: "flex",
-          minHeight: "50rem",
-          justifyContent: "center",
-        }}
-      >
-        <CircularProgress />
-      </Box>
-    );
-  }
+  return (
+    <Container sx={{ mt: "2rem" }}>
+      <MessageTypesTable />
+    </Container>
+  );
 }
 
 export default MessageTypes;

1939b5e05212db877e70987909514329875e2d61
René Monroy <rene.monroy@gmail.com>
2022-05-10 09:48:20 -0500

    Remove MessageType reducer

--- src/store/messageType/messageType.constants.js
+++
@@ -1,5 +0,0 @@
-export const INITIAL_STATE = {
-  messageType: {},
-  status: "idle",
-  error: null,
-};
--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { reducer as uiReducer } from "./ui/ui.slice";
-import { reducer as messageTypeReducer } from "./messageType/messageType.slice";
 import { reducer as messageTypesReducer } from "./messageTypes/messageTypes.slice";
 
 const store = configureStore({
   devTools: true,
   reducer: {
     ui: uiReducer,
-    messageType: messageTypeReducer,
     messageTypes: messageTypesReducer,
   },
 });

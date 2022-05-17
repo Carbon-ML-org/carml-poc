28c50520d08a955c77152a2a946775905918948c
René Monroy <rene.monroy@gmail.com>
2022-05-17 15:28:53 -0500

    Add Message Type button to app sidebar

--- src/components/AppSidebar/AppSidebar.jsx
+++ src/components/AppSidebar/AppSidebar.jsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { Button, List, ListItem } from "@mui/material";
 import AppUsername from "../AppUsername/AppUsername";
+import MessageTypeDialog from "../MessageTypeDialog/MessageTypeDialog";
 import { SAppSidebar, SAppSidebarHeader } from "./AppSidebar.styles";
 
 function AppSidebar() {
   const { sidebarIsOpen, sidebarWidth, headerHeight } = useSelector(
     (state) => state.ui
   );
+  const [messageTypeDialogIsOpen, openMessageTypeDialog] = useState(false);
+
+  function handleNewMessageType() {
+    openMessageTypeDialog(true);
+  }
+
+  function handleOnClose() {
+    openMessageTypeDialog(false);
+  }
 
   return (
     <SAppSidebar
@@ -22,11 +32,20 @@ function AppSidebar() {
       </SAppSidebarHeader>
       <List>
         <ListItem>
-          <Button disableElevation fullWidth variant="contained">
+          <Button
+            disableElevation
+            fullWidth
+            onClick={handleNewMessageType}
+            variant="contained"
+          >
             New Message Type
           </Button>
         </ListItem>
       </List>
+      <MessageTypeDialog
+        open={messageTypeDialogIsOpen}
+        onClose={handleOnClose}
+      />
     </SAppSidebar>
   );
 }
--- src/views/MessageTypes/MessageTypesTable.jsx
+++ src/views/MessageTypes/MessageTypesTable.jsx
@@ -1,26 +1,45 @@
 import React from "react";
-import { Card, CardContent } from "@mui/material";
+import { Box, Skeleton, Card, CardContent } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 
-function MessageTypesTable({ rows, columns }) {
-  return (
-    <Card>
-      <CardContent
-        sx={{ width: "100%", height: "400px" }}
-        style={{ padding: 0 }}
+function MessageTypesTable({ rows, columns, status }) {
+  if (status === "success") {
+    return (
+      <Card>
+        <CardContent
+          sx={{ width: "100%", height: "400px" }}
+          style={{ padding: 0 }}
+        >
+          <DataGrid
+            sx={{ border: 0 }}
+            columns={columns}
+            pageSize={10}
+            rows={rows}
+            rowsPerPageOptions={[10]}
+            checkboxSelection
+            disableSelectionOnClick
+          />
+        </CardContent>
+      </Card>
+    );
+  } else if (status === "failed") {
+    return <p>Not found!</p>;
+  } else {
+    return (
+      <Box
+        sx={{
+          alignItems: "center",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+        }}
       >
-        <DataGrid
-          sx={{ border: 0 }}
-          columns={columns}
-          pageSize={10}
-          rows={rows}
-          rowsPerPageOptions={[10]}
-          checkboxSelection
-          disableSelectionOnClick
-        />
-      </CardContent>
-    </Card>
-  );
+        <Skeleton width="71rem" />
+        <Skeleton width="71rem" />
+        <Skeleton width="71rem" />
+      </Box>
+    );
+  }
 }
 
 export default MessageTypesTable;
